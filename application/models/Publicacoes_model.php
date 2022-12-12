1ff4b3253c0b9aebf36560e86ecf310a56bc9d95
joaolucasjolr <joaolucas.jolr@gmail.com>
2022-12-12 16:44:18 -0300

    primeiro commit

---
+++ application/models/Publicacoes_model.php
@@ -0,0 +1,29 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Publicacoes_model extends CI_model {
+
+    public $id;
+    public $categoria;
+    public $titulo;
+    public $subtitulo;
+    public $conteudo;
+    public $data;
+    public $img;
+    public $user;
+
+	public function __construct(){
+        parent::__construct();
+    }
+	
+    public function destaques_home (){
+        $this->db->select('usuario.id as idautor,
+            usuario.nome, postagens.id, postagens.titulo, postagens.subtitulo, postagens.user, postagens.data, postagens.img');
+        $this->db->from('postagens');
+        $this->db->join('usuario', 'usuario.id = postagens.user');
+        $this->db->limit(4);
+        $this->db->order_by('postagens.data','DESC');
+        return $this->db->get('')->result();
+    }
+  
+}
